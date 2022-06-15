440917a018c94b44e2461ec3249483d1c0f27a8e
Trojan0101 <aloysius.mon.05@gmail.com>
2022-06-16 02:47:30 +0530

    Working on logic for active and inactive user sort

--- poc/poc/src/main/resources/static/js/scripts.js
+++ poc/poc/src/main/resources/static/js/scripts.js
@@ -68,37 +68,16 @@ function sortUserNamesAsc() {
   }
 }
 
-function sortActiveInactive() {
-  var list, i, switching, b, shouldSwitch, dir, switchcount = 0;
-  list = document.getElementById("myUL");
-  switching = true;
-  dir = "asc";
-  while (switching) {
-    switching = false;
-    b = list.getElementsByTagName("li");
-    for (i = 0; i < (b.length - 1); i++) {
-      shouldSwitch = false;
-      if (dir == "asc") {
-        if (b[i].innerHTML.toLowerCase() > b[i + 1].innerHTML.toLowerCase()) {
-          shouldSwitch = true;
-          break;
-        }
-      } else if (dir == "desc") {
-        if (b[i].innerHTML.toLowerCase() < b[i + 1].innerHTML.toLowerCase()) {
-          shouldSwitch = true;
-          break;
-        }
-      }
-    }
-    if (shouldSwitch) {
-      b[i].parentNode.insertBefore(b[i + 1], b[i]);
-      switching = true;
-      switchcount++;
-    } else {
-      if (switchcount == 0 && dir == "asc") {
-        dir = "desc";
-        switching = true;
-      }
-    }
-  }
-}
+// const activeList = activeUsers.split(",");
+// const inactiveList = inactiveUsers.split(",");
+// const fullList = activeList.concat(inactiveList);
+// console.log(fullList)
+//
+// function sortActiveInactive() {
+//   let list = document.getElementById("myUL");
+//   let b = list.getElementsByTagName("li");
+//
+//   for (let i = 0; i < (b.length); i++) {
+//     b[i].innerHTML = fullList[i];
+//   }
+// }
